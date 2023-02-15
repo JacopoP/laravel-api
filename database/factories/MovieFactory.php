698aa7f065ca075d58d5ca42dfbeae52ce833a31
Jac22 <jacopo.prudentino@gmail.com>
2023-02-15 16:18:46 +0100

    Completed factories

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->words(3, true),
+            'year' => fake()->year(),
+            'cashOut' => fake()->boolean()
+                        ? fake()->numberBetween(100, 2000000000)
+                        : NULL,
+        ];
+    }
+}
