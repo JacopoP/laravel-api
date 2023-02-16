9a16e44e0d513105bfa2506c4643e3cd3128976d
Jac22 <jacopo.prudentino@gmail.com>
2023-02-16 18:10:06 +0100

    Printed movies' titles in front-end

---
+++ front-end/vue-test/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script>
+import axios from 'axios';
+
+export default {
+
+  data() {
+    return {
+      movies: [],
+    }
+  },
+
+  mounted() {
+
+    axios.get('http://localhost:8000/api/v1/movie/all')
+      .then(res => {
+
+        const data = res.data;
+        const success = data.success;
+        this.movies = data.response;
+      })
+      .catch(err => console.error(err));
+  }
+};
+</script>
+
+<template><h2 v-for="movie in this.movies">{{ movie.name }}</h2></template>
+
+<style scoped></style>
